e4e1b35be73dfc5380f270605c9d959453070ef0
jo-jstrm <jambalaya_jo@posteo.ch>
2022-06-25 20:53:26 +0200

    Add minimal electron app using typescript, react, webpack

---
+++ ui/src/app.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import { displayText, Props } from './components/displayText'
+
+// Notice the ! -> https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+const container = document.getElementById('root')!
+const root = createRoot(container);
+
+const myText:Props = { text: "Hello from React" }
+
+root.render(displayText(myText));
+
