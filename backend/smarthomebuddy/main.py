115bb48fb1557e87ec1dadf3b69fc553ad3c3ccb
jo-jstrm <jambalaya_jo@posteo.ch>
2022-06-28 12:01:57 +0200

    Add minimal Python package

---
+++ backend/smarthomebuddy/main.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def main():
+    logging.info('Started Python backend.')
+
+
+if __name__ == '__main__':
+    main()
