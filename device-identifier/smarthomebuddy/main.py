b2f7c34203499d70e37e39425357b131b7385790
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2022-07-08 17:39:23 +0200

    Squashed commit of the following:
    commit 5f6d6a4f8c5120e3f9efa6d7aa1ad6e4f112e51a
    Author: jo-jstrm <jambalaya_jo@posteo.ch>
    Date:   Mon Jul 4 17:45:30 2022 +0200
    
        Add dev-setup.md
    
    commit 3cb996f97e80a34b2384c27d35a36e2029b428ce
    Author: jo-jstrm <jambalaya_jo@posteo.ch>
    Date:   Mon Jul 4 17:45:12 2022 +0200
    
        Add SASS to project
    
    commit 80eeae82f8fa384ddaee5234f75a2e4577c1a4ad
    Author: jo-jstrm <jambalaya_jo@posteo.ch>
    Date:   Thu Jun 30 12:56:28 2022 +0200
    
        Add local black settings in project setup
    
    commit a6ef7e8ac0aea92cb27c9cd101d06550e518f959
    Author: jo-jstrm <jambalaya_jo@posteo.ch>
    Date:   Thu Jun 30 12:20:07 2022 +0200
    
        Change folder names
    
    commit 115bb48fb1557e87ec1dadf3b69fc553ad3c3ccb
    Author: jo-jstrm <jambalaya_jo@posteo.ch>
    Date:   Tue Jun 28 12:01:57 2022 +0200
    
        Add minimal Python package
    
    commit e01584a5c84eeab2691b7e969639313e461bcd38
    Author: jo-jstrm <jambalaya_jo@posteo.ch>
    Date:   Tue Jun 28 12:01:42 2022 +0200
    
        Change name
    
    commit e4e1b35be73dfc5380f270605c9d959453070ef0
    Author: jo-jstrm <jambalaya_jo@posteo.ch>
    Date:   Sat Jun 25 20:53:26 2022 +0200
    
        Add minimal electron app using typescript, react, webpack

---
+++ device-identifier/smarthomebuddy/main.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def main():
+    logging.info('Started Python device-identifier.')
+
+
+if __name__ == '__main__':
+    main()
