067f701dde80462719218a1074cc83d73cdf30f3
jo-jstrm <jambalaya_jo@posteo.ch>
2022-07-12 16:38:29 +0200

    Add argparse and logging

--- device-identifier/smarthomebuddy/main.py
+++ device-identifier/smarthomebuddy/main.py
@@ -1,12 +1,28 @@
 import logging
+import argparse
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+
+def _set_log_level() -> None:
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+
+def _get_args() -> dict[
+    str,
+]:
+    parser = argparse.AgrumentParser(description="SmartHomeBuddie's device identifier.")
+    parser.add_argument(
+        "collect", dest="collect_mode", action="store_true", help="Collect network packages."
+    )
+    parser.set_defaults(collect_mode=False)
+    return parser.parse_args()
 
 
 def main():
-    logging.info('Started Python device-identifier.')
+    _set_log_level()
+    logging.info("Started Python device-identifier.")
+    args = _get_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
