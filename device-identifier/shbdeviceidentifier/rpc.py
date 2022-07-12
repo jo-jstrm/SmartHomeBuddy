2092e5cd27f25bf8f2adf021a57451060dfd1350
jo-jstrm <jambalaya_jo@posteo.ch>
2022-07-12 14:21:12 +0200

    Revert "Add exemplary zerorpc server for device-identifier"
    This reverts commit 63d7b650d32bf1139c730ef7642e78cd4120431a.

--- device-identifier/shbdeviceidentifier/rpc.py
+++
@@ -1,8 +0,0 @@
-class DeviceIdentifierApi:
-    def greet_user(self, name: str) -> str:
-        """Greet the user!"""
-        return f'Greetings from Python, {name}!'
-
-    def echo(self, text: str) -> str:
-        """Echo any text."""
-        return text
