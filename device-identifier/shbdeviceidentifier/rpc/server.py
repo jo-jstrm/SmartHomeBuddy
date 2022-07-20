e741d4426f02e728e84e18a405e16c85064b907c
jo-jstrm <jambalaya_jo@posteo.ch>
2022-07-20 21:46:31 +0200

    Update rpc server

--- device-identifier/shbdeviceidentifier/rpc/server.py
+++ device-identifier/shbdeviceidentifier/rpc/server.py
@@ -4,8 +4,10 @@ from .proto.heartbeat_pb2_grpc import HeartbeatServicer, add_HeartbeatServicer_t
 
 
 def run_rpc_server() -> None:
+    port = 8090
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     add_HeartbeatServicer_to_server(HeartbeatServicer(), server)
-    server.add_insecure_port("[::]:8090")
+    logging.info(f"Server listening on port {port}")
+    server.add_insecure_port(f"[::]:{port}")
     server.start()
     server.wait_for_termination()
