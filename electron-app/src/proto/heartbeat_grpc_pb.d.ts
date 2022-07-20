5562581c791c645b0f29e6b8658adc04b909cedc
jo-jstrm <jo-jstrm@users.noreply.github.com>
2022-07-20 19:17:16 +0000

    Prettified Code!

--- electron-app/src/proto/heartbeat_grpc_pb.d.ts
+++ electron-app/src/proto/heartbeat_grpc_pb.d.ts
@@ -1,4 +1,4 @@
-// package: 
+// package:
 // file: heartbeat.proto
 
 /* tslint:disable */
@@ -7,35 +7,89 @@
 import * as grpc from "grpc";
 import * as heartbeat_pb from "./heartbeat_pb";
 
-interface IHeartbeatService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
-    getHeartbeat: IHeartbeatService_IGetHeartbeat;
+interface IHeartbeatService
+  extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
+  getHeartbeat: IHeartbeatService_IGetHeartbeat;
 }
 
-interface IHeartbeatService_IGetHeartbeat extends grpc.MethodDefinition<heartbeat_pb.HeartbeatRequest, heartbeat_pb.HeartbeatResponse> {
-    path: "/Heartbeat/GetHeartbeat";
-    requestStream: false;
-    responseStream: false;
-    requestSerialize: grpc.serialize<heartbeat_pb.HeartbeatRequest>;
-    requestDeserialize: grpc.deserialize<heartbeat_pb.HeartbeatRequest>;
-    responseSerialize: grpc.serialize<heartbeat_pb.HeartbeatResponse>;
-    responseDeserialize: grpc.deserialize<heartbeat_pb.HeartbeatResponse>;
+interface IHeartbeatService_IGetHeartbeat
+  extends grpc.MethodDefinition<
+    heartbeat_pb.HeartbeatRequest,
+    heartbeat_pb.HeartbeatResponse
+  > {
+  path: "/Heartbeat/GetHeartbeat";
+  requestStream: false;
+  responseStream: false;
+  requestSerialize: grpc.serialize<heartbeat_pb.HeartbeatRequest>;
+  requestDeserialize: grpc.deserialize<heartbeat_pb.HeartbeatRequest>;
+  responseSerialize: grpc.serialize<heartbeat_pb.HeartbeatResponse>;
+  responseDeserialize: grpc.deserialize<heartbeat_pb.HeartbeatResponse>;
 }
 
 export const HeartbeatService: IHeartbeatService;
 
 export interface IHeartbeatServer {
-    getHeartbeat: grpc.handleUnaryCall<heartbeat_pb.HeartbeatRequest, heartbeat_pb.HeartbeatResponse>;
+  getHeartbeat: grpc.handleUnaryCall<
+    heartbeat_pb.HeartbeatRequest,
+    heartbeat_pb.HeartbeatResponse
+  >;
 }
 
 export interface IHeartbeatClient {
-    getHeartbeat(request: heartbeat_pb.HeartbeatRequest, callback: (error: grpc.ServiceError | null, response: heartbeat_pb.HeartbeatResponse) => void): grpc.ClientUnaryCall;
-    getHeartbeat(request: heartbeat_pb.HeartbeatRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: heartbeat_pb.HeartbeatResponse) => void): grpc.ClientUnaryCall;
-    getHeartbeat(request: heartbeat_pb.HeartbeatRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: heartbeat_pb.HeartbeatResponse) => void): grpc.ClientUnaryCall;
+  getHeartbeat(
+    request: heartbeat_pb.HeartbeatRequest,
+    callback: (
+      error: grpc.ServiceError | null,
+      response: heartbeat_pb.HeartbeatResponse
+    ) => void
+  ): grpc.ClientUnaryCall;
+  getHeartbeat(
+    request: heartbeat_pb.HeartbeatRequest,
+    metadata: grpc.Metadata,
+    callback: (
+      error: grpc.ServiceError | null,
+      response: heartbeat_pb.HeartbeatResponse
+    ) => void
+  ): grpc.ClientUnaryCall;
+  getHeartbeat(
+    request: heartbeat_pb.HeartbeatRequest,
+    metadata: grpc.Metadata,
+    options: Partial<grpc.CallOptions>,
+    callback: (
+      error: grpc.ServiceError | null,
+      response: heartbeat_pb.HeartbeatResponse
+    ) => void
+  ): grpc.ClientUnaryCall;
 }
 
 export class HeartbeatClient extends grpc.Client implements IHeartbeatClient {
-    constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
-    public getHeartbeat(request: heartbeat_pb.HeartbeatRequest, callback: (error: grpc.ServiceError | null, response: heartbeat_pb.HeartbeatResponse) => void): grpc.ClientUnaryCall;
-    public getHeartbeat(request: heartbeat_pb.HeartbeatRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: heartbeat_pb.HeartbeatResponse) => void): grpc.ClientUnaryCall;
-    public getHeartbeat(request: heartbeat_pb.HeartbeatRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: heartbeat_pb.HeartbeatResponse) => void): grpc.ClientUnaryCall;
+  constructor(
+    address: string,
+    credentials: grpc.ChannelCredentials,
+    options?: object
+  );
+  public getHeartbeat(
+    request: heartbeat_pb.HeartbeatRequest,
+    callback: (
+      error: grpc.ServiceError | null,
+      response: heartbeat_pb.HeartbeatResponse
+    ) => void
+  ): grpc.ClientUnaryCall;
+  public getHeartbeat(
+    request: heartbeat_pb.HeartbeatRequest,
+    metadata: grpc.Metadata,
+    callback: (
+      error: grpc.ServiceError | null,
+      response: heartbeat_pb.HeartbeatResponse
+    ) => void
+  ): grpc.ClientUnaryCall;
+  public getHeartbeat(
+    request: heartbeat_pb.HeartbeatRequest,
+    metadata: grpc.Metadata,
+    options: Partial<grpc.CallOptions>,
+    callback: (
+      error: grpc.ServiceError | null,
+      response: heartbeat_pb.HeartbeatResponse
+    ) => void
+  ): grpc.ClientUnaryCall;
 }
--- electron-app/src/proto/heartbeat_pb.d.ts
+++ electron-app/src/proto/heartbeat_pb.d.ts
@@ -1,4 +1,4 @@
-// package: 
+// package:
 // file: heartbeat.proto
 
 /* tslint:disable */
@@ -6,42 +6,64 @@
 
 import * as jspb from "google-protobuf";
 
-export class HeartbeatRequest extends jspb.Message { 
-    getCount(): number;
-    setCount(value: number): HeartbeatRequest;
+export class HeartbeatRequest extends jspb.Message {
+  getCount(): number;
+  setCount(value: number): HeartbeatRequest;
 
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): HeartbeatRequest.AsObject;
-    static toObject(includeInstance: boolean, msg: HeartbeatRequest): HeartbeatRequest.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: HeartbeatRequest, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): HeartbeatRequest;
-    static deserializeBinaryFromReader(message: HeartbeatRequest, reader: jspb.BinaryReader): HeartbeatRequest;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): HeartbeatRequest.AsObject;
+  static toObject(
+    includeInstance: boolean,
+    msg: HeartbeatRequest
+  ): HeartbeatRequest.AsObject;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: HeartbeatRequest,
+    writer: jspb.BinaryWriter
+  ): void;
+  static deserializeBinary(bytes: Uint8Array): HeartbeatRequest;
+  static deserializeBinaryFromReader(
+    message: HeartbeatRequest,
+    reader: jspb.BinaryReader
+  ): HeartbeatRequest;
 }
 
 export namespace HeartbeatRequest {
-    export type AsObject = {
-        count: number,
-    }
+  export type AsObject = {
+    count: number;
+  };
 }
 
-export class HeartbeatResponse extends jspb.Message { 
-    getAlive(): boolean;
-    setAlive(value: boolean): HeartbeatResponse;
+export class HeartbeatResponse extends jspb.Message {
+  getAlive(): boolean;
+  setAlive(value: boolean): HeartbeatResponse;
 
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): HeartbeatResponse.AsObject;
-    static toObject(includeInstance: boolean, msg: HeartbeatResponse): HeartbeatResponse.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: HeartbeatResponse, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): HeartbeatResponse;
-    static deserializeBinaryFromReader(message: HeartbeatResponse, reader: jspb.BinaryReader): HeartbeatResponse;
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): HeartbeatResponse.AsObject;
+  static toObject(
+    includeInstance: boolean,
+    msg: HeartbeatResponse
+  ): HeartbeatResponse.AsObject;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: HeartbeatResponse,
+    writer: jspb.BinaryWriter
+  ): void;
+  static deserializeBinary(bytes: Uint8Array): HeartbeatResponse;
+  static deserializeBinaryFromReader(
+    message: HeartbeatResponse,
+    reader: jspb.BinaryReader
+  ): HeartbeatResponse;
 }
 
 export namespace HeartbeatResponse {
-    export type AsObject = {
-        alive: boolean,
-    }
+  export type AsObject = {
+    alive: boolean;
+  };
 }
