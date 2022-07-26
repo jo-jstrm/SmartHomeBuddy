fb6b1a85fc3dd94ed2c9d7a7c0e1a3effe86daf3
jo-jstrm <jambalaya_jo@posteo.ch>
2022-07-26 14:33:37 +0200

    Distribute app as deb

---
+++ electron-app/src/rpc/DeviceIdentifier.ts
@@ -0,0 +1,15 @@
+import {spawn} from "child_process";
+import path from "path";
+
+
+export function runDeviceIdentifier(): void {
+  // Handle different location of the device-identifier binary
+  const isDevMode = process.env.DEV_MODE ? (process.env.DEV_MODE.trim() == 'true') : false;
+  console.log("DEV_MODE=" + isDevMode)
+  if (isDevMode) {
+    return
+  }
+  const deviceIdentifierPath = path.join(process.resourcesPath, "cli/cli")
+  console.log("deviceIdentifierPath: " + deviceIdentifierPath)
+  let child = spawn(deviceIdentifierPath);
+}
