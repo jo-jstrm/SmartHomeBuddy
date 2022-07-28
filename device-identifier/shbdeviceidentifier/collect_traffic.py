a085a7c2ce103d220b460d1928bb48a872ec732e
Tobias Becher <25723425+tb-devacc@users.noreply.github.com>
2022-07-28 14:34:42 +0200

    added live capture

--- device-identifier/shbdeviceidentifier/collect_traffic.py
+++ device-identifier/shbdeviceidentifier/collect_traffic.py
@@ -1,10 +1,11 @@
 # You can generate an API token from the "API Tokens Tab" in the UI
+import logging
 from typing import List
 
-import logging
 import pyshark
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
+from pyshark.capture.capture import Capture
 
 log = logging.getLogger("identifier")
 
@@ -14,14 +15,57 @@ bucket = "network-traffic"
 url = "http://localhost:8086"
 
 
-def collect_traffic():
-    # TODO
-    ...
+def collect_traffic(time: int = 10) -> bool:
+    cap = pyshark.LiveCapture(interface='eth0')
+    cap.sniff(timeout=time)
+    try:
+        write_cap_to_db(cap)
+    except Exception as e:
+        log.debug(e)
+        return False
+    return True
 
 
-def read_pcap(filepath: str, measurement: str = "packet") -> bool:
+def read_pcap(filepath: str) -> bool:
     cap = pyshark.FileCapture(filepath)
+    try:
+        write_cap_to_db(cap)
+    except Exception as e:
+        log.debug(e)
+        return False
+    return True
 
+
+def write_cap_to_db(cap: Capture, bucket: str = bucket, org: str = org, token: str = token, url: str = url,
+                    measurement: str = "packet") -> None:
+    """
+    The write_cap_to_db function writes a pyshark.capture.Capture object to an InfluxDB database
+        using the Line Protocol format (see https://v2.docs.influxdata.com/v2.0/write-data/#line-protocol).
+
+    Parameters
+    ----------
+        cap:Capture
+            A pyshark capture object containing packets to be written to the database.
+        bucket:str="network-traffic"
+            The name of the bucket in which data will be stored in InfluxDB, e.g., "network-traffic".
+        org:str="smarthomebuddy"
+            The name of the organization in which the bucket is stored in InfluxDB, e.g., "smarthomebuddy".
+        token:str=<user-token>
+            Authentication token for the influxdb api.
+        url:str="http://localhost:8086"
+            Url of the influxdb instance.
+        measurement:str="packet"
+            The name of the measurement.
+
+    Returns
+    -------
+
+        None
+
+    Doc Author
+    ----------
+        TB
+    """
     # create influx db Line Protocol sequence
     # one string per packet
     data: List[str] = []
@@ -49,7 +93,7 @@ def read_pcap(filepath: str, measurement: str = "packet") -> bool:
         if "tcp" in layer_names:
             tags["tcp_src"] = packet.tcp.port
             tags["tcp_dst"] = packet.tcp.dstport
-        elif "udp" in  layer_names:
+        elif "udp" in layer_names:
             tags["udp_src"] = packet.udp.port
             tags["udp_dst"] = packet.udp.dstport
 
@@ -73,15 +117,10 @@ def read_pcap(filepath: str, measurement: str = "packet") -> bool:
 
         data.append(line)
 
-    try:
-        write_to_influxdb(data)
-    except Exception as e:
-        log.debug(e)
-        return False
-    return True
+    write_to_influxdb(data=data, bucket=bucket, org=org, token=token, url=url)
 
 
-def write_to_influxdb(data: List[str]):
+def write_to_influxdb(data: List[str], bucket: str = bucket, org: str = org, token: str = token, url: str = url):
     with InfluxDBClient(url=url, token=token, org=org) as client:
         write_api = client.write_api(write_options=SYNCHRONOUS)
 
