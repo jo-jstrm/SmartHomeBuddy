e88939fef3175e4e9af7dc7be99392b89957ffb2
jo-jstrm <jambalaya_jo@posteo.ch>
2022-08-09 13:22:11 +0200

    Merge app.tsx and index.tsx

--- electron-app/src/index.tsx
+++ electron-app/src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
-import App from "./app";
+import Frame from "./views/Frame";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -9,6 +10,8 @@ root.render(
   <React.Fragment>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <BrowserRouter>
+      <Frame />
+    </BrowserRouter>
   </React.Fragment>
 );
