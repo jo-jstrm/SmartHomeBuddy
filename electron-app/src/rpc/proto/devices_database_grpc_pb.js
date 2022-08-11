c6be9860131d91e7c6bc46f5cb8547abcea3a5a5
jo-jstrm <jo-jstrm@users.noreply.github.com>
2022-08-11 22:43:57 +0000

    Prettified Code!

--- electron-app/src/rpc/proto/devices_database_grpc_pb.js
+++ electron-app/src/rpc/proto/devices_database_grpc_pb.js
@@ -1,57 +1,66 @@
 // GENERATED CODE -- DO NOT EDIT!
 
-'use strict';
-var grpc = require('@grpc/grpc-js');
-var devices_database_pb = require('./devices_database_pb.js');
+"use strict";
+var grpc = require("@grpc/grpc-js");
+var devices_database_pb = require("./devices_database_pb.js");
 
 function serialize_SmartHomeBuddy_ClassifyRequest(arg) {
   if (!(arg instanceof devices_database_pb.ClassifyRequest)) {
-    throw new Error('Expected argument of type SmartHomeBuddy.ClassifyRequest');
+    throw new Error("Expected argument of type SmartHomeBuddy.ClassifyRequest");
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_SmartHomeBuddy_ClassifyRequest(buffer_arg) {
-  return devices_database_pb.ClassifyRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return devices_database_pb.ClassifyRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_SmartHomeBuddy_ClassifyResponse(arg) {
   if (!(arg instanceof devices_database_pb.ClassifyResponse)) {
-    throw new Error('Expected argument of type SmartHomeBuddy.ClassifyResponse');
+    throw new Error(
+      "Expected argument of type SmartHomeBuddy.ClassifyResponse"
+    );
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_SmartHomeBuddy_ClassifyResponse(buffer_arg) {
-  return devices_database_pb.ClassifyResponse.deserializeBinary(new Uint8Array(buffer_arg));
+  return devices_database_pb.ClassifyResponse.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_SmartHomeBuddy_Devices(arg) {
   if (!(arg instanceof devices_database_pb.Devices)) {
-    throw new Error('Expected argument of type SmartHomeBuddy.Devices');
+    throw new Error("Expected argument of type SmartHomeBuddy.Devices");
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_SmartHomeBuddy_Devices(buffer_arg) {
-  return devices_database_pb.Devices.deserializeBinary(new Uint8Array(buffer_arg));
+  return devices_database_pb.Devices.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_SmartHomeBuddy_DevicesRequest(arg) {
   if (!(arg instanceof devices_database_pb.DevicesRequest)) {
-    throw new Error('Expected argument of type SmartHomeBuddy.DevicesRequest');
+    throw new Error("Expected argument of type SmartHomeBuddy.DevicesRequest");
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_SmartHomeBuddy_DevicesRequest(buffer_arg) {
-  return devices_database_pb.DevicesRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return devices_database_pb.DevicesRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
-
-var DevicesDatabaseService = exports.DevicesDatabaseService = {
+var DevicesDatabaseService = (exports.DevicesDatabaseService = {
   classifyDevices: {
-    path: '/SmartHomeBuddy.DevicesDatabase/ClassifyDevices',
+    path: "/SmartHomeBuddy.DevicesDatabase/ClassifyDevices",
     requestStream: false,
     responseStream: false,
     requestType: devices_database_pb.ClassifyRequest,
@@ -62,7 +71,7 @@ var DevicesDatabaseService = exports.DevicesDatabaseService = {
     responseDeserialize: deserialize_SmartHomeBuddy_ClassifyResponse,
   },
   getAllDevices: {
-    path: '/SmartHomeBuddy.DevicesDatabase/GetAllDevices',
+    path: "/SmartHomeBuddy.DevicesDatabase/GetAllDevices",
     requestStream: false,
     responseStream: false,
     requestType: devices_database_pb.DevicesRequest,
@@ -72,6 +81,8 @@ var DevicesDatabaseService = exports.DevicesDatabaseService = {
     responseSerialize: serialize_SmartHomeBuddy_Devices,
     responseDeserialize: deserialize_SmartHomeBuddy_Devices,
   },
-};
+});
 
-exports.DevicesDatabaseClient = grpc.makeGenericClientConstructor(DevicesDatabaseService);
+exports.DevicesDatabaseClient = grpc.makeGenericClientConstructor(
+  DevicesDatabaseService
+);
--- electron-app/src/rpc/proto/devices_database_pb.d.ts
+++ electron-app/src/rpc/proto/devices_database_pb.d.ts
@@ -9,18 +9,29 @@ export class ClassifyRequest extends jspb.Message {
 
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): ClassifyRequest.AsObject;
-  static toObject(includeInstance: boolean, msg: ClassifyRequest): ClassifyRequest.AsObject;
-  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-  static serializeBinaryToWriter(message: ClassifyRequest, writer: jspb.BinaryWriter): void;
+  static toObject(
+    includeInstance: boolean,
+    msg: ClassifyRequest
+  ): ClassifyRequest.AsObject;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: ClassifyRequest,
+    writer: jspb.BinaryWriter
+  ): void;
   static deserializeBinary(bytes: Uint8Array): ClassifyRequest;
-  static deserializeBinaryFromReader(message: ClassifyRequest, reader: jspb.BinaryReader): ClassifyRequest;
+  static deserializeBinaryFromReader(
+    message: ClassifyRequest,
+    reader: jspb.BinaryReader
+  ): ClassifyRequest;
 }
 
 export namespace ClassifyRequest {
   export type AsObject = {
-    param: string,
-  }
+    param: string;
+  };
 }
 
 export class ClassifyResponse extends jspb.Message {
@@ -29,18 +40,29 @@ export class ClassifyResponse extends jspb.Message {
 
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): ClassifyResponse.AsObject;
-  static toObject(includeInstance: boolean, msg: ClassifyResponse): ClassifyResponse.AsObject;
-  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-  static serializeBinaryToWriter(message: ClassifyResponse, writer: jspb.BinaryWriter): void;
+  static toObject(
+    includeInstance: boolean,
+    msg: ClassifyResponse
+  ): ClassifyResponse.AsObject;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: ClassifyResponse,
+    writer: jspb.BinaryWriter
+  ): void;
   static deserializeBinary(bytes: Uint8Array): ClassifyResponse;
-  static deserializeBinaryFromReader(message: ClassifyResponse, reader: jspb.BinaryReader): ClassifyResponse;
+  static deserializeBinaryFromReader(
+    message: ClassifyResponse,
+    reader: jspb.BinaryReader
+  ): ClassifyResponse;
 }
 
 export namespace ClassifyResponse {
   export type AsObject = {
-    isDone: boolean,
-  }
+    isDone: boolean;
+  };
 }
 
 export class DevicesRequest extends jspb.Message {
@@ -49,18 +71,29 @@ export class DevicesRequest extends jspb.Message {
 
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): DevicesRequest.AsObject;
-  static toObject(includeInstance: boolean, msg: DevicesRequest): DevicesRequest.AsObject;
-  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-  static serializeBinaryToWriter(message: DevicesRequest, writer: jspb.BinaryWriter): void;
+  static toObject(
+    includeInstance: boolean,
+    msg: DevicesRequest
+  ): DevicesRequest.AsObject;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: DevicesRequest,
+    writer: jspb.BinaryWriter
+  ): void;
   static deserializeBinary(bytes: Uint8Array): DevicesRequest;
-  static deserializeBinaryFromReader(message: DevicesRequest, reader: jspb.BinaryReader): DevicesRequest;
+  static deserializeBinaryFromReader(
+    message: DevicesRequest,
+    reader: jspb.BinaryReader
+  ): DevicesRequest;
 }
 
 export namespace DevicesRequest {
   export type AsObject = {
-    param: string,
-  }
+    param: string;
+  };
 }
 
 export class Device extends jspb.Message {
@@ -73,18 +106,26 @@ export class Device extends jspb.Message {
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): Device.AsObject;
   static toObject(includeInstance: boolean, msg: Device): Device.AsObject;
-  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-  static serializeBinaryToWriter(message: Device, writer: jspb.BinaryWriter): void;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: Device,
+    writer: jspb.BinaryWriter
+  ): void;
   static deserializeBinary(bytes: Uint8Array): Device;
-  static deserializeBinaryFromReader(message: Device, reader: jspb.BinaryReader): Device;
+  static deserializeBinaryFromReader(
+    message: Device,
+    reader: jspb.BinaryReader
+  ): Device;
 }
 
 export namespace Device {
   export type AsObject = {
-    name: string,
-    macAddress: string,
-  }
+    name: string;
+    macAddress: string;
+  };
 }
 
 export class Devices extends jspb.Message {
@@ -96,16 +137,23 @@ export class Devices extends jspb.Message {
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): Devices.AsObject;
   static toObject(includeInstance: boolean, msg: Devices): Devices.AsObject;
-  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-  static serializeBinaryToWriter(message: Devices, writer: jspb.BinaryWriter): void;
+  static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> };
+  static extensionsBinary: {
+    [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>;
+  };
+  static serializeBinaryToWriter(
+    message: Devices,
+    writer: jspb.BinaryWriter
+  ): void;
   static deserializeBinary(bytes: Uint8Array): Devices;
-  static deserializeBinaryFromReader(message: Devices, reader: jspb.BinaryReader): Devices;
+  static deserializeBinaryFromReader(
+    message: Devices,
+    reader: jspb.BinaryReader
+  ): Devices;
 }
 
 export namespace Devices {
   export type AsObject = {
-    devicesList: Array<Device.AsObject>,
-  }
+    devicesList: Array<Device.AsObject>;
+  };
 }
-
