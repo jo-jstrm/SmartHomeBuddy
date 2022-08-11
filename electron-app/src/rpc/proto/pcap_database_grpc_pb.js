7339732460f5b447c595ccd1ff63ddeb2b7398a5
jo-jstrm <jo-jstrm@users.noreply.github.com>
2022-08-11 07:52:04 +0000

    Prettified Code!

--- electron-app/src/rpc/proto/pcap_database_grpc_pb.js
+++ electron-app/src/rpc/proto/pcap_database_grpc_pb.js
@@ -1,35 +1,38 @@
 // GENERATED CODE -- DO NOT EDIT!
 
-'use strict';
-var grpc = require('@grpc/grpc-js');
-var pcap_database_pb = require('./pcap_database_pb.js');
+"use strict";
+var grpc = require("@grpc/grpc-js");
+var pcap_database_pb = require("./pcap_database_pb.js");
 
 function serialize_DbLoadRequest(arg) {
   if (!(arg instanceof pcap_database_pb.DbLoadRequest)) {
-    throw new Error('Expected argument of type DbLoadRequest');
+    throw new Error("Expected argument of type DbLoadRequest");
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_DbLoadRequest(buffer_arg) {
-  return pcap_database_pb.DbLoadRequest.deserializeBinary(new Uint8Array(buffer_arg));
+  return pcap_database_pb.DbLoadRequest.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
 function serialize_DbLoadResponse(arg) {
   if (!(arg instanceof pcap_database_pb.DbLoadResponse)) {
-    throw new Error('Expected argument of type DbLoadResponse');
+    throw new Error("Expected argument of type DbLoadResponse");
   }
   return Buffer.from(arg.serializeBinary());
 }
 
 function deserialize_DbLoadResponse(buffer_arg) {
-  return pcap_database_pb.DbLoadResponse.deserializeBinary(new Uint8Array(buffer_arg));
+  return pcap_database_pb.DbLoadResponse.deserializeBinary(
+    new Uint8Array(buffer_arg)
+  );
 }
 
-
-var PcapDatabaseService = exports.PcapDatabaseService = {
+var PcapDatabaseService = (exports.PcapDatabaseService = {
   loadPcapIntoDatabase: {
-    path: '/PcapDatabase/LoadPcapIntoDatabase',
+    path: "/PcapDatabase/LoadPcapIntoDatabase",
     requestStream: false,
     responseStream: false,
     requestType: pcap_database_pb.DbLoadRequest,
@@ -39,6 +42,7 @@ var PcapDatabaseService = exports.PcapDatabaseService = {
     responseSerialize: serialize_DbLoadResponse,
     responseDeserialize: deserialize_DbLoadResponse,
   },
-};
+});
 
-exports.PcapDatabaseClient = grpc.makeGenericClientConstructor(PcapDatabaseService);
+exports.PcapDatabaseClient =
+  grpc.makeGenericClientConstructor(PcapDatabaseService);
