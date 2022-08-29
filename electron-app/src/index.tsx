353f8bd0f6e446cd3daf417f6197db09aab2b6d7
Tobias Becher <25723425+tb-devacc@users.noreply.github.com>
2022-08-29 13:26:03 +0200

    rm redundant CssBaseline

--- electron-app/src/index.tsx
+++ electron-app/src/index.tsx
@@ -1,15 +1,12 @@
 import * as React from "react";
-import { createRoot } from "react-dom/client";
-import CssBaseline from "@mui/material/CssBaseline";
+import {createRoot} from "react-dom/client";
 import Frame from "./views/Frame";
-import { HashRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.Fragment>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
     <HashRouter>
       <Frame />
     </HashRouter>
