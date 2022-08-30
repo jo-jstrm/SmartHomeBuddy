e3730f9192f60f8c2ddbf05786371169245ef04e
Tobias Becher <25723425+tb-devacc@users.noreply.github.com>
2022-08-30 16:34:44 +0200

    changed to SHB

--- electron-app/src/components/common/Copyright.tsx
+++ electron-app/src/components/common/Copyright.tsx
@@ -4,14 +4,14 @@ import Typography from "@mui/material/Typography";
 export default function Copyright(props: any) {
   return (
     <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/jo-jstrm/SmartHomeBuddy">
+        SmartHomeBuddy
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
