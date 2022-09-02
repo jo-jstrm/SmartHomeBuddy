dcfcb982389f59388629c1028e68d1c68d1acca1
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2022-09-02 14:50:12 +0200

    Set up testing and refactor workflows (#86)
    * Set up testing and refactor workflows (#81)
    
    * Improve the GUI frame and common functionality (#85)
    
    * added views
    
    * rm secondary items
    
    * rm dashboard blueprint
    
    * added dynamic view title
    
    * rm redundant CssBaseline
    
    * rm notifications badge
    
    * added light/dark mode with toggle
    
    * improved themes
    
    * changed to SHB
    
    * Prettified Code!
    
    Co-authored-by: TB-DevAcc <TB-DevAcc@users.noreply.github.com>
    
    Co-authored-by: TB-DevAcc <TB-DevAcc@users.noreply.github.com>

--- electron-app/src/components/common/Copyright.tsx
+++ electron-app/src/components/common/Copyright.tsx
@@ -10,8 +10,8 @@ export default function Copyright(props: any) {
       {...props}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://github.com/jo-jstrm/SmartHomeBuddy">
+        SmartHomeBuddy
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
