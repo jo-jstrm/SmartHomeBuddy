d9c417f91ffb98309c5bd85612960a5f8e03e435
TB-DevAcc <tb-devacc@users.noreply.github.com>
2022-09-05 15:05:32 +0200

    fixed commands tests

--- device-identifier/tests/test_commands.py
+++ device-identifier/tests/test_commands.py
@@ -13,12 +13,9 @@ from shbdeviceidentifier.utilities.app_utilities import DATA_DIR
 class TestCommands:
     """
     Test class for the commands module.
-    Commands not included because they are tested through their contained functions:
-     - read
-     - run_rpc_server
     """
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture
     def caplog(self, caplog: LogCaptureFixture):
         handler_id = logger.add(caplog.handler, format="{message}")
         yield caplog
@@ -38,25 +35,26 @@ class TestCommands:
         assert db.is_connected()
         db.stop_InfluxDB()
 
-    def test_run_rpc_server(self, db):
+    def test_run_rpc_server(self):
+        # Create db instance, because fixture is not available in a separate process
+        db = Database()
         rpc_server_process = Process(target=run_rpc_server, args=(db,), daemon=True)
         rpc_server_process.start()
         time.sleep(2)
 
         # Send a SIGTERM to the process
         rpc_server_process.terminate()
-
-        # Check if the process and the database are still running
-        assert not rpc_server_process.is_alive()
-        assert not db.is_connected()
+        time.sleep(2)
 
         if rpc_server_process.is_alive():
             rpc_server_process.kill()
-            # Check again after SIGKILL
+            time.sleep(1)
+            # Check if the process and the database are still running
             assert not rpc_server_process.is_alive()
             assert not db.is_connected()
 
     def test_read(self, db, dummy_pcap, caplog):
+        db.start()
         read(db, dummy_pcap, "pcap")
         assert "Wrote" in caplog.text
         assert "Failed to write" not in caplog.text
