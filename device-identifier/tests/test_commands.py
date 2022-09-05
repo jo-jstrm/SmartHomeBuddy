def05f231b5d3b260631e9c92846c15519ea853e
TB-DevAcc <tb-devacc@users.noreply.github.com>
2022-09-05 12:16:37 +0200

    implemented remaining tests

--- device-identifier/tests/test_commands.py
+++ device-identifier/tests/test_commands.py
@@ -1,6 +1,13 @@
-import pytest
+import time
+from multiprocessing import Process
 
+import pytest
+from _pytest.logging import LogCaptureFixture
+from loguru import logger
+
+from shbdeviceidentifier.commands import start_database, run_rpc_server, read
 from shbdeviceidentifier.db import Database
+from shbdeviceidentifier.utilities.app_utilities import DATA_DIR
 
 
 class TestCommands:
@@ -12,9 +19,44 @@ class TestCommands:
     """
 
     @pytest.fixture(scope="class")
-    def fixture_db(self):
+    def caplog(self, caplog: LogCaptureFixture):
+        handler_id = logger.add(caplog.handler, format="{message}")
+        yield caplog
+        logger.remove(handler_id)
+
+    @pytest.fixture(scope="class")
+    def db(self):
         return Database()
 
-    def test_start_database(self, fixture_db):
-        fixture_db.start()
-        assert fixture_db.is_connected()
+    @pytest.fixture(scope="class")
+    def dummy_pcap(self):
+        return (DATA_DIR / "pcaps" / "dummy.pcap").as_posix()
+
+    def test_start_database(self, db):
+        start_database(db)
+        db.start()
+        assert db.is_connected()
+        db.stop_InfluxDB()
+
+    def test_run_rpc_server(self, db):
+        rpc_server_process = Process(target=run_rpc_server, args=(db,), daemon=True)
+        rpc_server_process.start()
+        time.sleep(2)
+
+        # Send a SIGTERM to the process
+        rpc_server_process.terminate()
+
+        # Check if the process and the database are still running
+        assert not rpc_server_process.is_alive()
+        assert not db.is_connected()
+
+        if rpc_server_process.is_alive():
+            rpc_server_process.kill()
+            # Check again after SIGKILL
+            assert not rpc_server_process.is_alive()
+            assert not db.is_connected()
+
+    def test_read(self, db, dummy_pcap, caplog):
+        read(db, dummy_pcap, "pcap")
+        assert "Wrote" in caplog.text
+        assert "Failed to write" not in caplog.text
