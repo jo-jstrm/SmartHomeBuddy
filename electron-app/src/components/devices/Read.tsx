7396d1026ef137730d646592c50a64c9e003498c
Johannes Jestram <17967115+jo-jstrm@users.noreply.github.com>
2022-09-17 17:40:50 +0200

    Add Read button

---
+++ electron-app/src/components/devices/Read.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
+import * as React from "react";
+import { callClassifyDevices } from "../../rpc/clients/DeviceDatabaseClient";
+import { styled } from "@mui/system";
+import Typography from "@mui/material/Typography";
+
+const StyledPaper = styled(Paper)(() => ({
+  margin: 10,
+  padding: 15,
+}));
+
+const StyledDiv = styled("div")(() => ({
+  display: "flex",
+  justifyContent: "space-between",
+  marginBottom: 5,
+  alignItems: "center",
+}));
+
+export default function Read() {
+  const [readStatus, setReadStatus] = useState(
+    "Read network data from a capture file."
+  );
+  const classifyDevices = () => {
+    setReadStatus("Reading capture file. This might take up to 15 minutes...");
+    callClassifyDevices()
+      .then(() => {
+        setReadStatus("Succefully read all data!");
+      })
+      .catch((err: Error) => {
+        console.error("Catch: " + err.toString());
+        setReadStatus("No response from Device Identifier.");
+      });
+  };
+  return (
+    <StyledPaper>
+      <StyledDiv>
+        <Button
+          variant="outlined"
+          size="medium"
+          color="primary"
+          onClick={classifyDevices}
+        >
+          Read Data from Capture File
+        </Button>
+        <Typography>{readStatus}</Typography>
+      </StyledDiv>
+    </StyledPaper>
+  );
+}
