97d7bc9d4e7818c2259ce84ba2449e22ce09f696
Johannes Jestram <17967115+jo-jstrm@users.noreply.github.com>
2022-09-20 20:10:14 +0200

    Move read functionality to network data tab

--- electron-app/src/views/Data.tsx
+++ electron-app/src/views/Data.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Copyright from "../components/common/Copyright";
+import Read from "../components/data/Read";
 
 export default function Data() {
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Read />
       <Copyright sx={{ pt: 4 }} />
     </Container>
   );
--- electron-app/src/views/Devices.tsx
+++ electron-app/src/views/Devices.tsx
@@ -3,12 +3,10 @@ import Container from "@mui/material/Container";
 import Classification from "../components/devices/Classification";
 import Copyright from "../components/common/Copyright";
 import DevicesGrid from "../components/devices/DevicesGrid";
-import Read from "../components/devices/Read";
 
 export default function Devices() {
   return (
     <Container maxWidth="lg" sx={{ my: 2 }}>
-      <Read />
       <Classification />
       <DevicesGrid />
       <Copyright sx={{ pt: 4 }} />
