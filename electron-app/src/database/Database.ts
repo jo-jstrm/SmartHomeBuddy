0c6d97598e6992460f96d82e76a8cb3ab6733ca4
Johannes Jestram <17967115+jo-jstrm@users.noreply.github.com>
2022-09-29 09:50:46 +0200

    Implement function for db.run

--- electron-app/src/database/Database.ts
+++ electron-app/src/database/Database.ts
@@ -32,3 +32,32 @@ export function queryAll(query: string, params: any[] = []): Promise<any> {
     }
   });
 }
+
+/**
+ * Query the SQLite DB.
+ * @param query: Query string
+ * @param params: Query parameters. Optional.
+ * @type {(query: string, params: any[]) => Promise<any>}
+ */
+export function queryRun(query: string, params: any[] = []): Promise<any> {
+  const isDevMode = process.env.DEV_MODE
+    ? process.env.DEV_MODE.trim() == "true"
+    : false;
+  let dbPath;
+  if (isDevMode) {
+    dbPath = config.database.dev_dir;
+  } else {
+    dbPath = path.join(homedir, config.database.dir);
+  }
+  console.log("SQLite DB path: " + dbPath);
+  const db = new Database(dbPath, {verbose: console.log});
+  return new Promise((accept, reject) => {
+    try {
+      accept(db.prepare(query).run(params));
+    } catch (err) {
+      console.log("Error running query: " + query);
+      console.log(err);
+      reject(err);
+    }
+  });
+}
