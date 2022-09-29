48b5d3a480db92e3a345075e9e9bef310020fe6a
Tobias Becher <25723425+tb-devacc@users.noreply.github.com>
2022-09-29 12:22:16 +0200

    add backdrop for device classification

--- electron-app/src/components/devices/Classification.tsx
+++ electron-app/src/components/devices/Classification.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import {useEffect, useState} from "react";
-import {Button, FormControl, InputLabel, MenuItem} from "@mui/material";
+import {useContext, useEffect, useState} from "react";
+import {Backdrop, Button, CircularProgress, FormControl, InputLabel, MenuItem} from "@mui/material";
 import Paper from "@mui/material/Paper";
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
 import Typography from "@mui/material/Typography";
 import {styled} from "@mui/system";
 import {callClassifyDevices} from "../../rpc/clients/DeviceDatabaseClient";
 import {queryAll} from "../../database/Database";
 import {DbMeasurement} from "../../types/DeviceTypes";
+import {FileContext} from "../common/FileContext";
 
 const StyledPaper = styled(Paper)(() => ({
   margin: 10,
@@ -23,20 +24,30 @@ const StyledDiv = styled("div")(() => ({
 
 export default function Classification() {
   const [classifierStatus, setClassifierStatus] = useState(
-    "Classify IoT devices based on the network data in your database."
+      "Classify IoT devices based on the network data in your database."
   );
   const [selectedMeasurement, setSelectedMeasurement] = useState("");
   const [uniqueMeasurements, setUniqueMeasurements] = useState([""]);
+
+  // Backdrop state
+  const {devicesBackdrop, setDevicesBackdrop} = useContext(FileContext)
+
   const classifyDevices = () => {
+    // Set backdrop and spinner to loading
+    setDevicesBackdrop(true);
+
+    // Classify devices
     setClassifierStatus("Classifying. This might take a moment...");
     const classifierModel = "random_forest"
     callClassifyDevices(classifierModel, selectedMeasurement)
-      .then(() => {
-        setClassifierStatus("Classified!");
-      })
-      .catch((err: Error) => {
-        console.error("Catch: " + err.toString());
-        setClassifierStatus("No response from Device Identifier.");
+        .then(() => {
+          setClassifierStatus("Classified!");
+          setDevicesBackdrop(false);
+        })
+        .catch((err: Error) => {
+          console.error("Catch: " + err.toString());
+          setClassifierStatus("No response from Device Identifier.");
+          setDevicesBackdrop(false);
       });
   };
   const queryMeasurements = (): void => {
@@ -70,14 +81,21 @@ export default function Classification() {
         >
           Classify Devices
         </Button>
-        <FormControl sx={{ minWidth: 150 }}>
+        <Backdrop
+            sx={{color: (theme) => theme.palette.primary.contrastText, zIndex: (theme) => theme.zIndex.fab}}
+            open={devicesBackdrop}
+            onClick={() => setDevicesBackdrop(false)}
+        >
+          <CircularProgress color="inherit"/>
+        </Backdrop>
+        <FormControl sx={{minWidth: 150}}>
           <InputLabel id="measurement-select-label">Measurement</InputLabel>
           <Select
-            labelId="measurement-select-label"
-            id="measurement-select"
-            value={selectedMeasurement}
-            label="Measurement"
-            onChange={handleMeasurement}
+              labelId="measurement-select-label"
+              id="measurement-select"
+              value={selectedMeasurement}
+              label="Measurement"
+              onChange={handleMeasurement}
           >
             {uniqueMeasurements.map((measurement, index) => {
               return (<MenuItem key={index} value={measurement}>{measurement}</MenuItem>);
