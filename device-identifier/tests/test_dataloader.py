e48a6f906904683a0236b9446852d73c01fdc45f
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2022-09-29 15:48:36 +0200

    Fixing Database tests and test workflow (#107)
    [...]
    
    * added pytest-check
    
    * improved influx_process handling
    
    * renamed unit tests
    
    * enable debug messages
    
    * split debug and report testing
    
    * fix run syntax error and split up tests
    
    * fix cd
    
    * fix cd
    
    * manual revert splitting debug and report testing

--- device-identifier/tests/test_dataloader.py
+++ device-identifier/tests/test_dataloader.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import pandas as pd
 import pytest
+import pytest_check as check
 
 from shbdeviceidentifier.commands import read
 from shbdeviceidentifier.dataloader import DataLoader
@@ -17,33 +18,33 @@ class TestDataLoader:
     def test_from_csv(self, file_path, request):
         file_path = request.getfixturevalue(file_path)
         df = DataLoader.from_csv(file_path)
-        assert isinstance(df, pd.DataFrame)
-        assert not df.empty
+        check.is_true(isinstance(df, pd.DataFrame))
+        check.is_true(not df.empty)
 
     @pytest.mark.parametrize("file_path", ["dummy_pcap"])
     def test_from_pcap(self, file_path, request):
         file_path = request.getfixturevalue(file_path)
         df = DataLoader.from_pcap(file_path)
-        assert isinstance(df, pd.DataFrame)
-        assert not df.empty
+        check.is_true(isinstance(df, pd.DataFrame))
+        check.is_true(not df.empty)
 
     @pytest.mark.skip(reason="Not implemented yet.")
     def test_from_generator(self):
         # TODO: implement
-        assert True
+        check.is_true(True)
 
     @pytest.mark.skip(reason="Not implemented yet.")
     def test_from_dict(self):
         # TODO: implement
-        assert True
+        check.is_true(True)
 
     @pytest.mark.parametrize("training_labels_path", ["dummy_labels_json"])
     def test_labels_from_json(self, training_labels_path, request):
         training_labels_path = request.getfixturevalue(training_labels_path)
         labels = DataLoader.labels_from_json(training_labels_path)
 
-        assert isinstance(labels, pd.DataFrame)
-        assert not labels.empty
+        check.is_true(isinstance(labels, pd.DataFrame))
+        check.is_true(not labels.empty)
 
     @pytest.mark.parametrize("training_data_path", ["dummy_pcap"])
     @pytest.mark.parametrize("training_labels_path", ["dummy_labels_json"])
@@ -54,10 +55,10 @@ class TestDataLoader:
         df, labels = DataLoader.from_file(training_data_path, training_labels_path, devices_to_train)
 
         if devices_to_train:
-            assert isinstance(df, pd.DataFrame)
-            assert isinstance(labels, pd.Series)
-            assert not df.empty
-            assert not labels.empty
+            check.is_true(isinstance(df, pd.DataFrame))
+            check.is_true(isinstance(labels, pd.Series))
+            check.is_true(not df.empty)
+            check.is_true(not labels.empty)
 
     @pytest.mark.parametrize("devices_to_train", [["Google-Nest-Mini", "ESP-1DC41C"]])
     @pytest.mark.parametrize("data_path", ["dummy_pcap"])
@@ -66,17 +67,21 @@ class TestDataLoader:
 
         db.start()
         read(db, data_path, "pcap")
-        train_df, train_labels = DataLoader.from_database(from_timestamp=EARLIEST_TIMESTAMP,
-                                                          to_timestamp=datetime.now(), measurement="main",
-                                                          bucket="network-traffic", devices_to_train=devices_to_train)
+        train_df, train_labels = DataLoader.from_database(
+            from_timestamp=EARLIEST_TIMESTAMP,
+            to_timestamp=datetime.now(),
+            measurement="main",
+            bucket="network-traffic",
+            devices_to_train=devices_to_train,
+        )
         db.stop()
 
         # TODO: improve this test
         # Since `from_database` can return (None, None) this is still correct,
         # but does not tell us much about the actual purpose of the method.
         if isinstance(train_df, pd.DataFrame):
-            assert isinstance(train_labels, pd.Series)
+            check.is_true(isinstance(train_labels, pd.Series))
             # TODO: make sure db contains values and test for empty
             # [...]
-            assert not train_df.empty
-            assert not train_labels.empty
+            check.is_true(not train_df.empty)
+            check.is_true(not train_labels.empty)
